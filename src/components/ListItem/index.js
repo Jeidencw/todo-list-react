4509f51e5e65248bcf48d749ed0293fa42f97343
Jeidencw <henriquecw22@gmail.com>
2023-05-26 12:59:32 -0300

    checked

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -6,6 +6,7 @@ const ListItem = ({task, handleRemove, handleOnEdit,id}) => {
     const [onEdit, setOnEdit] = useState(false);
     const [editVal, setEditVal] = useState(task);
     const myRef = React.createRef();
+    const [isChecked, setIsChecked] = useState(false);
 
     const onRemove = () => {
         myRef.current.className = "active"
@@ -38,11 +39,15 @@ const ListItem = ({task, handleRemove, handleOnEdit,id}) => {
         setOnEdit(false)
     }
 
+    const toggleChecked = () => {
+        setIsChecked(!isChecked);
+    }
+
     return(
 
         <>
             {onEdit ? (
-                <li>
+                 <li className={isChecked ? "checked" : ""} onClick={toggleChecked}>
                     <input type='text'  
                         value={editVal}
                         name='editVal'
@@ -55,7 +60,7 @@ const ListItem = ({task, handleRemove, handleOnEdit,id}) => {
                 </div>
             </li>
             ) : (
-                <li ref={myRef}>
+                <li ref={myRef} className={isChecked ? "checked" : ""} onClick={toggleChecked}>
                     {task}
                     <div className='row'>
                         <i className='fa fa-pencil' title='Edit' onClick={handleEdit}></i>
